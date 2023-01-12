eea0075d3495551a602c502384eea808fa4e66f5
nomso <chinomsokoye@gmail.com>
2023-01-12 00:48:20 +0000

    Write SQL script to rank country origin of bands, ordered by number of (non-unique) fans

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- Write SQL script that ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column must be origin and nb_fans
+-- Script can be executed on any database
+
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands GROUP BY origin
+ORDER BY nb_fans DESC;
