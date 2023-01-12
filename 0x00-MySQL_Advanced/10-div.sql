69a30eb58a3410041cd3461218d0a97459cb67fa
nomso <chinomsokoye@gmail.com>
2023-01-12 12:21:20 +0000

    Crreate function SafeDiv divides (and returns) the first by the second or return 0 if second is equal to 0

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Create function safeDiv that divides (and returns) the first by the second number
+-- or returns 0 if second number is equal to 0
+-- function SafeDiv takes 2 arguments: a, INT, b, INT
+-- returns a / b or 0 if b == 0
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER ;
